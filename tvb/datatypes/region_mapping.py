b4e0fca18692b8c29831c44960680aa132f6be18
Alister Johnson <ajohnson@cs.uoregon.edu>
2018-05-15 16:15:07 -0700

    RegionVolumeMapping also subclasses Connectivity, but I'm not sure it should

--- tvb/datatypes/region_mapping.py
+++ tvb/datatypes/region_mapping.py
@@ -59,8 +59,6 @@ class RegionMapping(Connectivity):
 
     mapping = numpy.array([])
 
-    connectivity = Connectivity
-
     surface = Surface
 
     __generate_table__ = True
@@ -126,19 +124,17 @@ class RegionMapping(Connectivity):
         summary = super(RegionMapping, self)._find_summary_info()
         summary.update({"Source Surface": self.surface.display_name,
                         "Source Surface GID": self.surface.gid,
-                        "Connectivity GID": self.connectivity.gid,
-                        "Connectivity": self.connectivity.display_name})
+                        "Connectivity GID": self.gid,
+                        "Connectivity": self.display_name})
         return summary
 
 
-class RegionVolumeMapping(arrays.MappedArray):
+class RegionVolumeMapping(Connectivity):
     """
     Each value is representing the index in Connectivity regions to which the current voxel is mapped.
     """
 
-    array_data = numpy.array([])
-
-    connectivity = Connectivity
+    mapping = numpy.array([])
 
     volume = Volume
 
@@ -158,7 +154,7 @@ class RegionVolumeMapping(arrays.MappedArray):
         LOG.info("Writing RegionVolumeMapping with min=%d, mix=%d" % (data.min(), data.max()))
         if self.apply_corrections:
             data = numpy.array(data, dtype=numpy.int32)
-            data[data >= self.connectivity.number_of_regions] = -1
+            data[data >= self.number_of_regions] = -1
             data[data < -1] = -1
             LOG.debug("After corrections: RegionVolumeMapping min=%d, mix=%d" % (data.min(), data.max()))
 
@@ -185,10 +181,10 @@ class RegionVolumeMapping(arrays.MappedArray):
             if not_matched:
                 LOG.warn("Not matched regions will be considered background: %s" % not_matched)
 
-        if data.min() < -1 or data.max() >= self.connectivity.number_of_regions:
+        if data.min() < -1 or data.max() >= self.number_of_regions:
             raise exceptions.ValidationException("Invalid Mapping array: [%d ... %d]" % (data.min(), data.max()))
 
-        self.store_data("array_data", data)
+        self.store_data("mapping", data)
 
 
     def get_voxel_region(self, x_plane, y_plane, z_plane):
@@ -197,7 +193,7 @@ class RegionVolumeMapping(arrays.MappedArray):
         slices = slice(x_plane, x_plane + 1), slice(y_plane, y_plane + 1), slice(z_plane, z_plane + 1)
         voxel = self.read_data_slice(slices)[0, 0, 0]
         if voxel != -1:
-            return self.connectivity.region_labels[int(voxel)]
+            return self.region_labels[int(voxel)]
         else:
             return 'background'
 
@@ -209,11 +205,11 @@ class RegionVolumeMapping(arrays.MappedArray):
 
         if mapped_array_slice:
             matrix_slice = parse_slice(mapped_array_slice)
-            measure = mapped_array.get_data('array_data', matrix_slice)
+            measure = mapped_array.get_data('mapping', matrix_slice)
         else:
-            measure = mapped_array.get_data('array_data')
+            measure = mapped_array.get_data('mapping')
 
-        if measure.shape != (self.connectivity.number_of_regions, ):
+        if measure.shape != (self.number_of_regions, ):
             raise ValueError('cannot project measure on the space')
 
         result_x = measure[slice_x]
@@ -237,6 +233,6 @@ class RegionVolumeMapping(arrays.MappedArray):
         summary = super(RegionVolumeMapping, self)._find_summary_info()
         summary.update({"Source Volume": self.volume.display_name,
                         "Source Volume GID": self.volume.gid,
-                        "Connectivity GID": self.connectivity.gid,
-                        "Connectivity": self.connectivity.display_name})
+                        "Connectivity GID": self.gid,
+                        "Connectivity": self.display_name})
         return summary
